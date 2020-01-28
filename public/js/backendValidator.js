75b670d005ee6e76306d92ec83169ae3aa637784
Robert Weikert <robertw3256@gmail.com>
2020-01-28 20:12:14 +0100

    Update backendValidator.js
    Match is not a function. Test return true/false.

--- public/js/backendValidator.js
+++ public/js/backendValidator.js
@@ -1,41 +1,49 @@
-const validator = require('../public/js/backendValidator');
 
 class Validator {
 
     static isValid(name){
-        return !name.match(/(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script/);
+        //console.log(" isValid: " + !/(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script/.test(name))
+        return !/(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script/.test(name);
     }
 
     static isValidId(id){
-        return !id.match(/^\d+$/);
+        //console.log(" isValidId: " + /^\d+$/.test(id));
+        return /^\d+$/.test(id);
     }
 
     static isDateValid(date){
-        return date.match(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/);
+        //console.log(" isDateValid: " + /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(date));
+        return /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(date);
     }
     
-    static isEmailValid(date){
-        return date.match(/^[a-zA-Z0-9]+@[a-zA-Z0-9]{1,6}\.(pl|com)$/);
+    static isEmailValid(email){
+        //console.log(" isEmailValid: " + /^[a-zA-Z0-9]+@[a-zA-Z0-9]{1,6}\.(pl|com)$/.test(email));
+        return /^[a-zA-Z0-9]+@[a-zA-Z0-9]{1,6}\.(pl|com)$/.test(email);
     } 
 
     static isValueValid(value){
-        return value.match(/^([1-9]{1}[0-9]{1})$/);
+        //console.log(" isValueValid: " + /^([1-9]{1}[0-9]{1})$/.test(value));
+        return /^([1-9]{1}[0-9]{1})$/.test(value);
     }  
 
     static isCapacityValid(capacity){
-        return capacity.match(/^[0-9]{1,5}\.[0-9]{1}$/);
+        //console.log(" isCapacityValid: " + /^[0-9]{1,5}\.[0-9]{1}$/.test(capacity));
+        return /^[0-9]{1,5}\.[0-9]{1}$/.test(capacity);
     } 
 
     static isPriceValid(price){
-        return price.match(/^[0-9]{1,7}\.[0-9]{1,2}$/);
+        //console.log(" isPriceValid: " + /^[0-9]{1,7}\.[0-9]{1,2}$/.test(price));
+        return /^[0-9]{1,7}\.[0-9]{1,2}$/.test(price);
     } 
 
     static isPicturePathValid(path){
-        return path.match(/(https?:\/\/.*\.(?:png|jpg))/);
+        //console.log(" isPicturePathValid: " + /(https?:\/\/.*\.(?:png|jpg))/.test(path));
+        return /(https?:\/\/.*\.(?:png|jpg))/.test(path);
     } 
 
     static isPasswordValid(password){
-        return password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})(https?:\/\/.*\.(?:png|jpg))/);
+        //console.log(" isPasswordValid: " + /^\$2[ayb]\$.{56}$/.test(password));
+        return /^\$2[ayb]\$.{56}$/.test(password);
     }   
 }
 
